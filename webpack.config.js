234f3adf74a3a80386687ff9ddb95f7e91b526b0
Jens Larsen <jenslarsenslottis@gmail.com>
2024-05-30 19:50:18 +0200

    Updated file names and readme

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,22 @@ const path = require('path');
 
 module.exports = {
   entry: {
-    app: './src/app/app.js',
+    app: './src/app/app.jsx',
     background: './src/app/background.js',
-    gamefeed: './src/app/modules/game-schedule-feed.js',
+    gamefeed: './src/app/modules/game-schedule-feed.jsx',
     blockSpoilersYoutubeHomePage: './src/app/content_scripts/block-spoilers-youtube-home-page.js',
     blockSpoilersYoutubeWatchPage: './src/app/content_scripts/block-spoilers-youtube-watch-page.js',
-    blockSpoilersYoutubeSearchPage: './src/app/content_scripts/block-spoilers-youtube-search-page.js'
+    blockSpoilersYoutubeSearchPage: './src/app/content_scripts/block-spoilers-youtube-search-page.js',
+    nbaDataFetcher: './src/app/api/nba-data-fetcher.js',
+    timeConverter: './src/app/api/time-converter.js',
+    dataSplitter: './src/app/modules/data-splitter.js',
+    dbHandler: './src/app/modules/db-handler.js',
+    gameStructure: './src/app/modules/game-structure.js',
+    horizontalDatePicker: './src/app/modules/horizontal-date-picker.js',
+    skeleton: './src/app/modules/mui-react-modules/skeleton/skeleton.jsx',
+    dateCalendar: './src/app/modules/mui-react-modules/date-calendar/date-calendar.jsx',
+    customTheme: './src/app/modules/mui-react-modules/date-calendar/custom-theme/custom-theme.jsx',
+    customToolbar: './src/app/modules/mui-react-modules/date-calendar/custom-toolbar/custom-toolbar.jsx'
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -16,7 +26,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.jsx?$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
@@ -24,9 +34,10 @@ module.exports = {
             presets: ['@babel/preset-env', '@babel/preset-react']
           }
         }
-      }
-    ]
-  }
-};
-
-
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],  // Ensures Webpack picks up both JS and JSX files
+}
+}  
